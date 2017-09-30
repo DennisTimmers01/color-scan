f1c3c28b9721b6d1c883b26aba0a6213bbbf6aaa
Dennis Timmers <dennis@timmersnet.nl>
2017-09-30 14:21:21 +0200

    removed color extraction

--- src/App.js
+++ src/App.js
@@ -1,70 +1,57 @@
 import React, { Component } from 'react'
 import nearestColor from 'nearest-color'
-import Vibrant from 'node-vibrant'
 import NearestColorForm from './components/NearestColorForm'
-import ExtractColorForm from './components/ExtractColorForm'
 
 class App extends Component {
   constructor (props) {
     super(props)
     this.state = {
-      colors: {
-        red: '#f00',
-        yellow: '#ff0',
-        blue: '#00f'
+      colorPalettes: [
+        {
+          name: 'autumn',
+          colors: {
+            DarkMuted: '#543829',
+            DarkVibrant: '#581609',
+            LightMuted: '#bf8b71',
+            LightVibrant: '#f4b484',
+            Muted: '#986b59',
+            Vibrant: '#b66e55'
+          }
+        }
+      ],
+      vibrantColors: {
+        autumn: '#b66e55'
       },
-      vibrantColors: ['rgb(243, 22, 10)'],
       nearestColorValue: '',
-      nearestColorResult: '',
-      imageUrl: 'http://weknowyourdreams.com/images/autumn/autumn-02.jpg'
+      nearestColorResult: ''
     }
 
     this.changeHandler = this.changeHandler.bind(this)
     this.getNearestColor = this.getNearestColor.bind(this)
-    this.extractColorFromImage = this.extractColorFromImage.bind(this)
   }
 
   getNearestColor (e) {
     e.preventDefault()
-    const { colors, nearestColorValue } = this.state
-    const newNearestColor = nearestColor.from(colors)(nearestColorValue)
+    const { vibrantColors, nearestColorValue } = this.state
+    const newNearestColor = nearestColor.from(vibrantColors)(nearestColorValue)
     this.setState({
       nearestColorResult: newNearestColor.value,
       nearestColorValue: ''
     })
   }
 
-  extractColorFromImage (e) {
-    e.preventDefault(e)
-    const { imageUrl, vibrantColors } = this.state
-    Vibrant.from(imageUrl).getPalette()
-      .then(palette => this.convertToRGB(palette.Vibrant._rgb[0], palette.Vibrant._rgb[1], palette.Vibrant._rgb[2]))
-      .then(palette => this.setState({
-        vibrantColors: [...vibrantColors, palette]
-      }))
-  }
-
   changeHandler ({ target }) {
     this.setState({ [target.name]: target.value })
   }
 
-  convertToRGB (r, g, b) {
-    return `rgb(${r}, ${g}, ${b})`
-  }
-
   render () {
-    const { nearestColorValue, imageUrl } = this.state
+    const { nearestColorValue } = this.state
     return (
       <div className='App'>
         <NearestColorForm
           value={nearestColorValue}
           onChange={this.changeHandler}
           onSubmit={this.getNearestColor}
-        />
-        <ExtractColorForm
-          value={imageUrl}
-          onChange={this.changeHandler}
-          onSubmit={this.extractColorFromImage}
         />
       </div>
     )
